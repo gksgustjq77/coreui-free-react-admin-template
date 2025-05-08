4c81a9d4ac9648ed200eeccde0c0ed5090579cb8
gksgustjq77 <gksgustjq77@gmail.com>
2025-05-08 10:19:56 +0900

    feat : conver Utill 함수 추가

---
+++ src/utill/converter.ts
@@ -0,0 +1,26 @@
+export const convertTimestampToKorean = (timestamp: number): string => {
+  const date = new Date(timestamp)
+  const year = date.getFullYear()
+  const month = String(date.getMonth() + 1).padStart(2, '0')
+  const day = String(date.getDate()).padStart(2, '0')
+  const hours = String(date.getHours()).padStart(2, '0')
+  const minutes = String(date.getMinutes()).padStart(2, '0')
+  const seconds = String(date.getSeconds()).padStart(2, '0')
+
+  return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`
+}
+
+export const convertTimestampToKoreanHalf = (timestamp: number): any => {
+  const date = new Date(timestamp)
+  const year = date.getFullYear()
+  const month = String(date.getMonth() + 1).padStart(2, '0')
+  const day = String(date.getDate()).padStart(2, '0')
+  const hours = String(date.getHours()).padStart(2, '0')
+  const minutes = String(date.getMinutes()).padStart(2, '0')
+  const seconds = String(date.getSeconds()).padStart(2, '0')
+
+  return {
+    yearMonthDay: `${year}-${month}-${day}`,
+    time: `${hours}:${minutes}:${seconds}`,
+  }
+}
