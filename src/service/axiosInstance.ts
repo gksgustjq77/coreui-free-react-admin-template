2aee779959068d8f5a897f29ed47fb0a6def324a
gksgustjq77 <gksgustjq77@gmail.com>
2025-05-08 20:44:55 +0900

    fix : 토큰 만료시 로그인 페이지로 이동

--- src/service/axiosInstance.ts
+++ src/service/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const axiosInstance = axios.create({
-  baseURL: 'http://hejdev1.goqual.com:8080',
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   timeout: 50000,
 })
 
@@ -26,6 +26,7 @@ axiosInstance.interceptors.response.use(
     return response.data
   },
   async (error) => {
+    if (error.response?.status === 401) window.location.href = '/login'
     return Promise.reject(error.response?.data)
   },
 )
