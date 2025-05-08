5a0a283daa7a4f630551fb7da7f4fc2366e361cf
gksgustjq77 <gksgustjq77@gmail.com>
2025-05-08 21:04:56 +0900

    fix : 토큰만료 리다이렉트 수정

--- src/service/axiosInstance.ts
+++ src/service/axiosInstance.ts
@@ -26,7 +26,9 @@ axiosInstance.interceptors.response.use(
     return response.data
   },
   async (error) => {
-    if (error.response?.status === 401) window.location.href = '/login'
+    if (error.response?.status === 401 && !error.config.url?.includes('/api/auth/login')) {
+      window.location.href = '/login#/login'
+    }
     return Promise.reject(error.response?.data)
   },
 )
